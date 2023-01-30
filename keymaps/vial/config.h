56a6c8818976b9f9014c03060122acd98e80d21d
caseyclement <60848722+caseyclement@users.noreply.github.com>
2023-01-30 09:24:09 -0500

    Add files via upload

---
+++ keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x44, 0x3E, 0xA1, 0x7B, 0xF9, 0x08, 0xA6, 0x20}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
